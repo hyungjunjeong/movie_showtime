42ece9e597ae43ce85f96cb3d0178cfe560e1286
puss <jun.jeong@thinxtra.com>
2020-11-22 01:10:18 +1100

    basic feature done

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import { getModule } from "vuex-module-decorators";
+import MovieModule from "./modules/MovieModule";
 
-Vue.use(Vuex)
+interface Store {
+  MovieModule: MovieModule;
+}
+Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+const store = new Vuex.Store<Store>({
+  mutations: {},
+  actions: {},
   modules: {
-  }
-})
+    MovieModule,
+  },
+});
+
+export default store;
+
+// Vuex modules and store must be linked for actions to work properly.
+getModule(MovieModule, store);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid class="d-flex justify-center align-center">
+    <v-col cols="5" justify="center">
+      <SearchBar />
+      <MainTab />
+    </v-col>
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import MainTab from "@/components/MainTab.vue";
+import SearchBar from "@/components/SearchBar.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+@Component({
+  components: { MainTab, SearchBar },
+})
+export default class HomeView extends Vue {}
 </script>
